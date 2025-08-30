306855c0aa0b8b88aa4ee5bf012813ad88687d33
W.Yuening <793021821@qq.com>
2025-08-31 00:10:50 +0800

    feat: docker 配置

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "正在构建并启动Club Admin服务..."
 docker-compose up --build -d
 
 echo "服务启动完成！"
-echo "前端访问地址: http://localhost"
+echo "前端访问地址: http://localhost:8888"
 echo "后端API地址: http://localhost:3001"
 echo ""
 echo "查看服务状态: docker-compose ps"
