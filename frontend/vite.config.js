00bb271f8fe2dfa0be181efa963dfbc24c9193ea
qiaoxin111 <44386524+qiaoxin111@users.noreply.github.com>
2025-09-01 16:44:31 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  server: { proxy: { '/api': 'http://localhost:4000' } }
+  server: { proxy: { '/api': 'http://localhost:4000' }, allowedHosts: ["centos.iqiaoxin.life"] }
 });
